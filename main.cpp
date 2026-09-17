7725f4358abb88dd0946cab25aa34c4dd60a907a
suhAnton1934 <onton_vrn@mail.ru>
2026-09-17 12:08:51 +0300

    new algo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 int main(){
-    std::cout << "123" << std::endl;
+    int n;
+    std::cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; ++i){
+        std::cin >> arr[i];
+    }
+
+    for (int i = 1; i < n; ++i){
+        int j, key;
+        key = arr[i];
+        j = i-1;
+        while ((j >= 0) && (arr[j] > key)){
+            arr[j+1] = arr[j];
+            --j;
+        }
+        arr[j+1] = key;
+    }
+    for (int i = 0; i < n ;++i){
+        std::cout << arr[i] << " ";
+    }
     return 0;
 }
